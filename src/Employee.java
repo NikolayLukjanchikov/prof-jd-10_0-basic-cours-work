56dfc676f09527c663c89e34d133f05af5c59bae
Николай Лукьянчиков <nicoolas.ru@gmail.com>
2022-08-26 09:59:48 +0300

    Переделал equals (проверка только ФИО)

--- src/Employee.java
+++ src/Employee.java
@@ -20,12 +20,12 @@ public class Employee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return id == employee.id && Objects.equals(fio, employee.fio);
+        return Objects.equals(fio, employee.fio);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, fio);
+        return Objects.hash(fio);
     }
 
     @Override
